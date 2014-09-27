355520396dc2dbc7a7d0cbedf638f845477ec74d
J H <hanclansolo@J-Hs-MacBook-Pro.local>
2014-09-27 03:09:57 -0700

    Updates.

--- js/resume.js
+++ js/resume.js
@@ -32,47 +32,6 @@
 			var source = $("#experienceTemplate").text();
 			var template = _.template(source);
 
-			$("canvas.company-logo").livequery(
-				// created
-				function() {
-					if (this.dataset.url) {
-						var canvas = this;
-						var logo = new Image(); 
-						logo.src = this.dataset.url;
-						logo.onload = function() {
-							var context = canvas.getContext('2d');
-							var scale = 32;
-							var height = scale;
-							var width = Math.round(( scale / logo.height ) * logo.width);
-
-							canvas.width = width;
-							canvas.height = height;
-							context.drawImage(logo, 0, 0, width, height);
-
-							var imageData = context.getImageData(0, 0, width, height);
-	        		var data = imageData.data;
-
-        			// sample found online for converting to grayscale
-	        		for(var i = 0; i < data.length; i += 4) {
-			          var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-			          // red
-			          data[i] = brightness;
-			          // green
-			          data[i + 1] = brightness;
-			          // blue
-			          data[i + 2] = brightness;
-			        }
-
-			        context.putImageData(imageData, 0, 0);
-						};
-					}
-				}, 
-				// destroyed
-				function() {
-					
-				}
-			);
-
 			_.each(experiences, function(experience, name) {
 				experience.name = name;
 				
